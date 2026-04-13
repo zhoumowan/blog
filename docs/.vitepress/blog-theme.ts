049a0d7308e3c5a14e39c8fea549b6a305005de4
zhoumowan <fanzong211@126.com>
2026-04-13 13:03:32 +0800

    docs: 评论系统

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -39,11 +39,11 @@ const blogTheme = getThemeConfig({
     icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48"><g fill="none" stroke-linecap="round" stroke-width="4"><path fill="#2F88FF" stroke="#000" stroke-linejoin="round" d="M44 8H4V38H19L24 43L29 38H44V8Z"/><path stroke="#fff" d="M21 15L20 32"/><path stroke="#fff" d="M28 15L27 32"/><path stroke="#fff" d="M33 20L16 20"/><path stroke="#fff" d="M32 27L15 27"/></g></svg>',
     type: "giscus",
     options: {
-      repo: "WkndNite/blog",
+      repo: "zhoumowan/blog",
       repoId: "R_kgDONtQ1pA",
       category: "Announcements",
       categoryId: "DIC_kwDONtQ1pM4CnEEJ",
-      inputPosition: "top",
+      inputPosition: "bottom",
     },
     mobileMinify: true,
   },
